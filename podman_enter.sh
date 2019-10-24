0c10ef8cd678ce0278314bacf9f69849f1873acb
Kyle Flores <6565728+kyflores@users.noreply.github.com>
2019-10-23 23:49:20 -0400

    Let user specify directory to mount in container

--- podman_enter.sh
+++ podman_enter.sh
@@ -1,5 +1,12 @@
+if [ $# -eq 0 ]
+  then
+    echo "Please supply a directory to mount at /host!"
+	echo "podman_enter.sh ~/my/photos/folder"
+	exit 1
+fi
+
 podman run -it --rm \
-	-v $(pwd):/host \
+	-v $1:/host \
 	--security-opt label=disable \
 	--security-opt seccomp=unconfined \
 	--device=/dev/kfd \
